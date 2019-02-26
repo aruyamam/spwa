0834f3412ffbbc12934674970cb9e0fb73ea5d61
aruyamam <aruyamam@gmail.com>
2019-02-26 14:53:55 +0900

    4.4.2 チャットを変更する

--- src/js/spa.chat.js
+++ src/js/spa.chat.js
@@ -6,22 +6,95 @@ import { setConfigMap } from './spa.util';
 
 const configMap = {
    mainHtml: `
-      <div style="padding:1em; color:#fff;">
-         Say hello to chat
+      <div class="spa-chat">
+         <div class="spa-chat-head">
+            <div class="spa-chat-head__toggle"></div>
+            <div class="spa-chat-head__title">
+               Chat
+            </div>
+         </div>
+         <div className="spa-chat-closer">x</div>
+         <div className="spa-chat-sizer">
+            <div className="spa-chat-msgs"></div>
+            <div className="spa-chat-box">
+               <input type="text"/>
+               <div>send</div>
+            </div>
+         </div>
       </div>
    `,
-   settableMap: {},
+
+   settableMap: {
+      sliderOpenTime: true,
+      sliderCloseTime: true,
+      sliderOpenedEm: true,
+      sliderClosedEm: true,
+      sliderClosedTitle: true,
+      sliderOpenedTitle: true,
+
+      chatModel: true,
+      peopleModel: true,
+      setChatAnchor: true,
+   },
+
+   sliderOpenTime: 250,
+   sliderCloseTime: 250,
+   sliderOpenedEm: 16,
+   sliderClosedEm: 2,
+   sliderOpenedTitle: 'Click to close',
+   sliderClosedTitle: 'Click to open',
+
+   chatMoel: null,
+   peopleModel: null,
+   setChatAnchor: null,
 };
-const stateMap = { $container: null };
-const jqueryMap = {};
+const stateMap = {
+   $appendTarget: null,
+   positionType: 'closed',
+   pxPerEm: 0,
+   sliderHiddenPx: 0,
+   sliderClosedPx: 0,
+   sliderOpenedPx: 0,
+};
+let jqueryMap = {};
+
+// ------------------- BEGIN UTILITY METHODS ------------------
+const getEmSize = elem => Number(getComputedStyle(elem, '').fontSize.match(/\d*\.?\d*/)[0]);
+// -------------------- END UTILITY METHODS -------------------
 
 // --------------------- BEGIN DOM METHODS --------------------
 // Begin DOM method /setJqueryMap/
 const setJqueryMap = () => {
-   const { $container } = stateMap;
-   jqueryMap.$container = $container;
+   const { $appendTarget } = stateMap;
+   const $slider = $appendTarget.find('.spa-chat');
+
+   jqueryMap = {
+      $slider,
+      $head: $slider.find('.spa-chat-head'),
+      $toggle: $slider.find('.spa-chat-head__toggle'),
+      $title: $slider.find('.spa-chat-head__title'),
+      $sizer: $slider.find('.spa-chat-sizer'),
+      $msgs: $slider.find('.spa-chat-msgs'),
+      $box: $slider.find('.spa-chat-box'),
+      $input: $slider.find('.spa-chat-input input[type=text]'),
+   };
 };
 // End DOM method /setJqueryMap/
+
+// Begin DOM method /setPxSizes/
+const setPxSizes = () => {
+   const pxPerEm = getEmSize(jqueryMap.$slider.get(0));
+
+   const openedHeightEm = configMap.sliderOpenedEm;
+
+   stateMap.pxPerEm = pxPerEm;
+   stateMap.sliderClosedPx = configMap.sliderClosedEm * pxPerEm;
+   stateMap.sliderOpenedPx = openedHeightEm * pxPerEm;
+   jqueryMap.$sizer.css({
+      height: (openedHeightEm - 2) * pxPerEm,
+   });
+};
+// End DOM method /setPxSizes/
 
 // Begin public method /setSliderPosition/
 // Example   : spa.chat.setSliderPosition( 'closed' );
@@ -41,9 +114,74 @@ const setJqueryMap = () => {
 //   * false - The requested position was not achieved
 // Throws    : none
 //
+const setSliderPosition = (positionType, callback) => {
+   let heightPx;
+   let animateTime;
+   let sliderTitle;
+   let toggleText;
 
+   // return true if slider already in requested position
+   if (stateMap.positionType === positionType) {
+      return true;
+   }
+
+   // prepare animate parameters
+   switch (positionType) {
+      case 'opened':
+         heightPx = stateMap.sliderOpenedPx;
+         animateTime = configMap.sliderOpenTime;
+         sliderTitle = configMap.sliderOpenedTitle;
+         toggleText = '=';
+         break;
+
+      case 'hidden':
+         heightPx = 0;
+         animateTime = configMap.sliderOpenTime;
+         sliderTitle = '';
+         toggleText = '+';
+         break;
+
+      case 'closed':
+         heightPx = stateMap.sliderClosedPx;
+         animateTime = configMap.sliderCloseTime;
+         sliderTitle = configMap.sliderClosedTitle;
+         toggleText = '+';
+         break;
+
+      // bail for unknown positionType
+      default:
+         return false;
+   }
+
+   // animate slider position change
+   stateMap.positionType = '';
+   jqueryMap.$slider.animate({ height: heightPx }, animateTime, () => {
+      jqueryMap.$toggle.prop('title', sliderTitle);
+      jqueryMap.$toggle.text(toggleText);
+      stateMap.positionType = positionType;
+      if (callback) {
+         callback(jqueryMap.$slider);
+      }
+   });
+
+   return true;
+};
 // End public DOM method /setSliderPosition/
 // ---------------------- END DOM METHODS ---------------------
+
+// ------------------- BEGIN EVENT HANDLERS -------------------
+const onClickToggle = () => {
+   const { setChatAnchor } = configMap;
+   if (stateMap.positionType === 'opened') {
+      setChatAnchor('closed');
+   }
+   else if (stateMap.positionType === 'closed') {
+      setChatAnchor('opened');
+   }
+
+   return false;
+};
+// -------------------- END EVENT HANDLERS --------------------
 
 // ------------------- BEGIN PUBLIC METHODS -------------------
 // Example   : spa.chat.configModule({ slider_open_em : 18 });
@@ -93,10 +231,16 @@ const configModule = (inputMap) => {
 // Returns    : true on success, false on failure
 // Throws     : none
 //
-const initModule = ($container) => {
-   $container.html(configMap.mainHtml);
-   stateMap.$container = $container;
+const initModule = ($appendTarget) => {
+   $appendTarget.append(configMap.mainHtml);
+   stateMap.$appendTarget = $appendTarget;
    setJqueryMap();
+   setPxSizes();
+
+   // initialize chat slider to default title and state
+   jqueryMap.$toggle.prop('title', configMap.sliderClosedTitle);
+   jqueryMap.$head.click(onClickToggle);
+   stateMap.positionType = 'closed';
 
    return true;
 };
@@ -105,4 +249,5 @@ const initModule = ($container) => {
 export default {
    configModule,
    initModule,
+   setSliderPosition,
 };
