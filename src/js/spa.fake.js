91776ecde0f7edb3f2c3671b13d104dac9259611
aruyamam <aruyamam@gmail.com>
2019-02-28 14:48:48 +0900

    5.4.1 偽のユーザーリストを作成する

---
+++ src/js/spa.fake.js
@@ -0,0 +1,40 @@
+const getPeopleList = () => [
+   {
+      name: 'Betty',
+      _id: 'id_01',
+      cssMap: {
+         top: 20,
+         left: 20,
+         'background-color': 'rgb(128, 128, 128)',
+      },
+   },
+   {
+      name: 'Mike',
+      _id: 'id_02',
+      cssMap: {
+         top: 60,
+         left: 20,
+         'background-color': 'rgb(128, 255, 128)',
+      },
+   },
+   {
+      name: 'Pebbles',
+      _id: 'id_03',
+      cssMap: {
+         top: 100,
+         left: 20,
+         'background-color': 'rgb(128, 192, 192)',
+      },
+   },
+   {
+      name: 'Wilma',
+      _id: 'id_04',
+      cssMap: {
+         top: 140,
+         left: 20,
+         'background-color': 'rgb(192, 128, 128)',
+      },
+   },
+];
+
+export default getPeopleList;
